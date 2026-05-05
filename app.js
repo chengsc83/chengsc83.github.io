d413ac297a4c4603dee34bc1ad41ba85eca81bcb
鄭世駿 <93769905+chengsc83@users.noreply.github.com>
2026-05-05 23:25:11 +0800

    Update version label from 2.5 BETA to 2.5

--- app.js
+++ app.js
@@ -6096,7 +6096,7 @@ const App = {
                         </button>
                     </div>
                 </div>
-                <div class="p-4 text-center text-xs text-slate-500 border-t border-[var(--border-color)]">辯時計 2.5 BETA <br> 技術，為了更好的思辯</div>
+                <div class="p-4 text-center text-xs text-slate-500 border-t border-[var(--border-color)]">辯時計 2.5 <br> 技術，為了更好的思辯</div>
         `;
     },
 
