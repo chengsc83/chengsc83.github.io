da4f3edbb692f494a35e2d5669132ad667a16603
鄭世駿 <93769905+chengsc83@users.noreply.github.com>
2026-05-20 16:38:30 +0800

    Update service worker to version 2.5.16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'debate-clock-v2.5.15';
+const CACHE_NAME = 'debate-clock-v2.5.16';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -20,7 +20,7 @@ const ASSETS_TO_CACHE = [
 
 // 安裝 Service Worker 並快取靜態資源
 self.addEventListener('install', (event) => {
-  console.log('SW: Installing v2.5.15...');
+  console.log('SW: Installing v2.5.16...');
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return Promise.allSettled(
